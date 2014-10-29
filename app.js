b0637e1e76a6f34f1de902aa378ec5022ab7171a
David <xiao303178394@gmail.com>
2014-10-29 14:00:05 +0800

    新增＼列表＼首页对接库完成

--- app.js
+++ app.js
@@ -1,8 +1,13 @@
 var express = require('express');
 var path = require('path');
 var bodyParser = require('body-parser');
+var mongoose = require('mongoose');
+var Book = require('./models/book');
+var _ = require('underscore');
 var port = process.env.PORT || 3022;
 var app = express();
+
+mongoose.connect('mongodb://localhost/wDBook');
 
 app.set('views','./views/pages');
 app.set('view engine','jade');
@@ -12,109 +17,49 @@ app.use(bodyParser());
 app.use(express.static(path.join(__dirname,'public')));
 
 app.listen(port);
+//设置moment为全局内置对象
 app.locals.moment = require('moment');
 
 console.log('wDBook started on port ' + port);
 
 //路由定义
 app.get('/',function(req,res){
-	res.render('index',{
-		title:'wDBook ---首页',
-		books:[{
-			_id:1,
-			title:'干法',
-			cover:'http://epaper.bjnews.com.cn/images/2010-06/26/C09/c09626cb002.jpg'
-		},{
-			_id:1,
-			title:'干法',
-			cover:'http://epaper.bjnews.com.cn/images/2010-06/26/C09/c09626cb002.jpg'
-		},{
-			_id:1,
-			title:'干法',
-			cover:'http://epaper.bjnews.com.cn/images/2010-06/26/C09/c09626cb002.jpg'
-		},{
-			_id:1,
-			title:'干法',
-			cover:'http://epaper.bjnews.com.cn/images/2010-06/26/C09/c09626cb002.jpg'
+	Book.fetch(function(err,books){
+		if(err){
+			console.log(err);
 		}
-		]
+		res.render('index',{
+			title:'wDBook ---首页',
+			books:books
+		});
 	});
 });
 app.get('/admin/list',function(req,res){
-	res.render('list',{
-		title:'wDBook ---列表页',
-		books:[{
-			_id:1,
-			title:'干法',
-			author:'稻盛和夫',
-			country:'日本',
-			language:'汉语',
-			year:'2010',
-			meta:{
-				createTime:'20141028'
-			},
-			summary:'介绍稻盛和夫老先生所理解的人生的意义及通过努力工作改变人生的看法',
-			cover:'http://epaper.bjnews.com.cn/images/2010-06/26/C09/c09626cb002.jpg'
-		},{
-			_id:2,
-			title:'干法',
-			author:'稻盛和夫',
-			country:'日本',
-			language:'汉语',
-			year:'2010',
-			meta:{
-				createTime:'20141028'
-			},
-			summary:'介绍稻盛和夫老先生所理解的人生的意义及通过努力工作改变人生的看法',
-			cover:'http://epaper.bjnews.com.cn/images/2010-06/26/C09/c09626cb002.jpg'
-		},{
-			_id:3,
-			title:'干法',
-			author:'稻盛和夫',
-			country:'日本',
-			language:'汉语',
-			year:'2010',
-			meta:{
-				createTime:'20141028'
-			},
-			summary:'介绍稻盛和夫老先生所理解的人生的意义及通过努力工作改变人生的看法',
-			cover:'http://epaper.bjnews.com.cn/images/2010-06/26/C09/c09626cb002.jpg'
-		},{
-			_id:4,
-			title:'干法',
-			author:'稻盛和夫',
-			country:'日本',
-			language:'汉语',
-			year:'2010',
-			meta:{
-				createTime:'20141028'
-			},
-			summary:'介绍稻盛和夫老先生所理解的人生的意义及通过努力工作改变人生的看法',
-			cover:'http://epaper.bjnews.com.cn/images/2010-06/26/C09/c09626cb002.jpg'
+	Book.fetch(function(err,books){
+		if(err){
+			console.log(err);
 		}
-		]
+		res.render('list',{
+			title:'wDBook ---列表页',
+			books:books
+		});
 	});
 });
 app.get('/book/:id',function(req,res){
-	res.render('detail',{
-		title:'wDBook ---详情页',
-		book:{
-			_id:1,
-			title:'干法',
-			author:'稻盛和夫',
-			country:'日本',
-			language:'汉语',
-			year:'2010',
-			summary:'介绍稻盛和夫老先生所理解的人生的意义及通过努力工作改变人生的看法',
-			cover:'http://epaper.bjnews.com.cn/images/2010-06/26/C09/c09626cb002.jpg'
-		}
+	var id = req.params.id;
+	Book.findById(id,function(err,book){
+	console.log(book);
+		res.render('detail',{
+			title:'wDBook ---'+book.title+'详情页',
+			book:book
+		});
 	});
 });
 app.get('/admin/book',function(req,res){
 	res.render('admin',{
 		title:'wDBook ---后台录入页面',
 		book:{
-			_id:1,
+			_id:'',
 			title:'',
 			author:'',
 			country:'',
@@ -125,4 +70,41 @@ app.get('/admin/book',function(req,res){
 			cover:''
 		}
 	});
+});
+app.post('/admin/book/new',function(req,res){
+	var id = req.body.book._id;
+	var bookObj = req.body.book;
+	var _book;
+	if(typeof(id) !== 'undefined'){
+		Book.findById(function(err,book){
+			if(err){
+				console.log(err);
+			}
+
+			_book = _.extend(book,bookObj);
+			_book.save(function(err,book){
+				if (err) {
+					console.log(err);
+				}
+				res.redirect('/book/'+book._id);
+			});
+		});
+	} else {
+		_book = new Book({
+			author : bookObj.author,
+			title : bookObj.title,
+			language : bookObj.language,
+			country : bookObj.country,
+			summary : bookObj.summary,
+			flash : bookObj.flash,
+			cover : bookObj.cover,
+			year : bookObj.year
+		});
+		_book.save(function(err,book){
+			if (err) {
+				console.log(err);
+			}
+			res.redirect('/book/'+book._id);
+		});
+	}
 });
---
+++ schemas/book.js
@@ -0,0 +1,62 @@
+var mongoose = require('mongoose');
+var Schema = mongoose.Schema;
+var ObjectId = Schema.Types.ObjectId;
+
+var BookSchema = new Schema({
+  author: String,
+  title: String,
+  language: String,
+  country: String,
+  summary: String,
+  flash: String,
+  cover: String,
+  year: Number,
+  // pv: {
+  //   type: Number,
+  //   default: 0
+  // },
+  // category: {
+  //   type: ObjectId,
+  //   ref: 'Category'
+  // },
+  meta: {
+    createUser:String,
+    updateUser:String,
+    createTime: {
+      type: Date,
+      default: Date.now()
+    },
+    updateTime: {
+      type: Date,
+      default: Date.now()
+    }
+  }
+});
+
+// var ObjectId = mongoose.Schema.Types.ObjectId
+BookSchema.pre('save', function(next) {
+  if (this.isNew) {
+    this.meta.createTime = this.meta.updateTime = Date.now();
+  }
+  else {
+    this.meta.updateTime = Date.now();
+  }
+
+  next();
+});
+
+BookSchema.statics = {
+  fetch: function(cb) {
+    return this
+      .find({})
+      .sort('meta.updateTime')
+      .exec(cb);
+  },
+  findById: function(id, cb) {
+    return this
+      .findOne({_id: id})
+      .exec(cb);
+  }
+};
+
+module.exports = BookSchema;
